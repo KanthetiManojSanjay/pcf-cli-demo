4cc55740640d0edf762b0b47f4549632e0b55a09
Sanjay <kanthetisanjay7@gmail.com>
2019-09-08 21:22:07 +0530

    PCF Initial Commit

---
+++ src/main/java/com/pcf/demo/PcfCliDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pcf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfCliDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfCliDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pcf/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.pcf.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pcf")
+public class DemoController {
+	
+	@GetMapping("/demo")
+	public String getMessage() {
+		return "My App in PCF!!";
+	}
+
+}
